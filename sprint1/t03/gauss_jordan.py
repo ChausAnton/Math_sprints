7d9a8d76f7933398bebb5acb6c77e0de35092b81
Anton <georgesavr6@gmail.com>
2021-05-01 18:01:43 +0300

    jacobi

---
+++ sprint1/t03/gauss_jordan.py
@@ -0,0 +1,15 @@
+import numpy 
+
+def gauss_jordan(A, B):
+    pass
+
+if __name__ == "__main__":
+    myA=[
+    [3, -2, 4],
+    [3, 4, -2],
+    [2, -1, -1]
+    ]
+    myB = [21, 9, 10]
+    Gauss(myA, myB)
+    
+    
