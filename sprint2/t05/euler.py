db878e4cc65063ecd683b74b5e865561eeec8020
Anton <georgesavr6@gmail.com>
2021-05-06 19:06:49 +0300

    euler

---
+++ sprint2/t05/euler.py
@@ -0,0 +1,19 @@
+import math
+
+def func(x, y):
+    return ((x ** 2) - (2 * y))
+
+def euler(f, a, b, Yo, N):
+    res = []
+    h = (b - a) / N
+    y = Yo
+    x = a
+    for i in range(1, N + 1):
+        y = y + (h * f(x, y))
+        x = a + (i * h)
+        res.append(y)
+    return res
+    
+
+if __name__ == "__main__":
+    print(euler(func, 0, 1, 1, 10))
