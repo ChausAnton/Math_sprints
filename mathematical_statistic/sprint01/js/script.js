3deec26bdcec5a499b135249881cbc5be32d9d93
Anton <georgesavr6@gmail.com>
2021-05-23 20:54:25 +0300

    task2

--- mathematical_statistic/sprint01/js/script.js
+++ mathematical_statistic/sprint01/js/script.js
@@ -40,7 +40,60 @@ document.querySelector("#Calculate").addEventListener('click', () => {
     let samples = getSamples()
     let res1 = task1(samples)
     document.querySelector('.main_div .task1').innerHTML = res1['html'].innerHTML
-
+    
+    document.querySelector('.main_div .task2').innerHTML = containers_for_task2(samples).innerHTML
     task2(res1['VariesNear'])
-        //document.querySelector('.main_div .task2').innerHTML = task2(samples).innerHTML
 })
+
+
+function containers_for_task2(samples) {
+    const container = document.createElement('div')
+    for (let i = 0; i < samples.length; i++) {
+        const div = document.createElement('div')
+        div.classList.add('chart_' + String.fromCharCode(i + 97))
+        div.classList.add('chart')
+
+        const sampleSpan = document.createElement('span')
+        sampleSpan.classList.add('text_sample_name')
+        sampleSpan.classList.add('text')
+        sampleSpan.innerHTML = "Sample " + String.fromCharCode(i + 97).toUpperCase() + ":"
+
+        const span1 = document.createElement('span')
+        span1.classList.add('text')
+        span1.classList.add('Polygon_text')
+        span1.innerHTML = "Polygon chart:"
+
+        const div1 = document.createElement('div')
+        div1.classList.add('Polygon')
+        div1.classList.add('Chart')
+
+        const span2 = document.createElement('span')
+        span2.classList.add('text')
+        span2.classList.add('Histogram_text')
+        span2.innerHTML = "Histogram chart:"
+
+        const div2 = document.createElement('div')
+        div2.classList.add('Histogram')
+        div2.classList.add('Chart')
+
+        const span3 = document.createElement('span')
+        span3.classList.add('text')
+        span3.classList.add('Empirical_text')
+        span3.innerHTML = "Empirical distribution function chart:"
+
+        const div3 = document.createElement('div')
+        div3.classList.add('Empirical')
+        div3.classList.add('Chart')
+
+        div1.appendChild(span1)
+        div2.appendChild(span2)
+        div3.appendChild(span3)
+        div.appendChild(sampleSpan)
+        div.appendChild(div1)
+        div.appendChild(div2)
+        div.appendChild(div3)
+        container.appendChild(div)
+    }
+
+    return container
+}
--- mathematical_statistic/sprint01/js/task2.js
+++ mathematical_statistic/sprint01/js/task2.js
@@ -1,3 +1,100 @@
-export default (VariesNear) => {
-    console.log(VariesNear)
+export default (varies_near) => {
+    destroy_charts();
+    console.log(varies_near[0][0])
+   
+
+    for(let i = 0; i < varies_near.length; i++) {
+        let first_class = ".chart_" + String.fromCharCode(i + 97)
+        print_chart(varies_near[i][0],varies_near[i][1], "chart" + i,"Polygon of transmission frequencies", "result_chart", first_class + " .Polygon", "id",1);
+
+        let h = Math.abs(varies_near[i][0][0] - varies_near[i][0][varies_near[i][0].length-1]);
+        let h_arr = new Array(varies_near[i][0].length);
+        h_arr.fill(h);
+    
+    
+        let xh_arr = new Array(varies_near[i][1].length);
+        for (let z = 0; z < varies_near[i][1].length; z++) {
+           xh_arr[z] = varies_near[i][1][z] / h;
+        }
+        print_chart(xh_arr, h_arr, "chart_" + i,"Histogram of frequencies", "result_chart", first_class + " .Histogram", "id",2);
+        
+    
+        print_chart(varies_near[i][0], varies_near[i][4], "chart3" + i,"Histogram of frequencies", "result_chart", first_class + " .Empirical", "id",2); 
+        
+    }
+}
+
+function print_chart(x_arr,y_arr,id,  label,class_name, parent, mode, chart_mode) {
+    let chart_div = document.createElement("div");
+    chart_div.innerHTML = '<canvas id="' + id +'" class = "'+ class_name +'"></canvas>';
+    
+    
+    if (mode == "id") {
+        document.querySelector(parent).appendChild(chart_div); 
+    } else if (mode == "class") {
+        document.getElementsByClassName(parent)[0].appendChild(chart_div); 
+    } else if (mode == "body") {
+        document.body.appendChild(chart_div); 
+    }
+
+    var speedCanvas = document.getElementById(id);
+
+   if (chart_mode == 1) {
+        var data_chart = {
+        labels: x_arr,
+        datasets: [{
+            label: label,
+            data: y_arr,
+            lineTension: 0,
+            fill: false,
+            borderColor: 'black',
+            backgroundColor: "white"
+        },
+    ]
+        };
+
+        var chartOptions = {
+        legend: {
+            display: true,
+            position: 'top',
+
+        }};
+
+        var lineChart = new Chart(speedCanvas, {
+            type: 'line',
+            data: data_chart,
+            options: chartOptions
+        });
+        charts.push(lineChart);
+    }
+    if (chart_mode == 2) {
+        var densityData = {
+            label: label,
+            data: x_arr,
+            backgroundColor: [
+                'rgba(0, 99, 132, 0.6)'
+            ]
+          };
+           
+          var barChart = new Chart(speedCanvas, {
+            type: 'bar',
+            data: {
+              labels: y_arr,
+              datasets: [densityData]
+            }
+          })
+          charts.push(barChart);
+    }
+
+
+}
+
+let charts = [];
+
+function destroy_charts() {
+    let size = charts.length;
+    console.log(charts);
+    for (let i = 0; i < size; i++) {
+        charts[i].destroy();
+    }
 }
